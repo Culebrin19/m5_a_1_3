5d528fb5455b8bc3afcddb8aa658a4d693526479
Culebrin19 <150950663+Culebrin19@users.noreply.github.com>
2023-12-21 14:12:39 +0100

    commit des de Harsh

--- src/main/java/local/caparrella/Main.java
+++ src/main/java/local/caparrella/Main.java
@@ -2,19 +2,6 @@ package local.caparrella;
 
 public class Main {
     public static void main(String[] args) {
-        // Crea un array d'objectes Persona
-        Persona[] persones = new Persona[3];
-
-        // Inicialitza els objectes a l'array
-        persones[0] = new Persona("Joan", 25);
-        persones[1] = new Persona("Maria", 30);
-        persones[1] = new Persona("Pepe", 30);
-      
-
-        // Accedeix als objectes a l'array
-        for (Persona persona : persones) {
-            System.out.println("Nom: " + persona.getNom() + ", Edat: " + persona.getEdat());
-            System.out.println("Harsh");
+            System.out.println("El meu nom es Harsh, tinc 19 anys");
         }
     }
-}
